4678970309ac1827c3723e679769f93d7be82ab4
davschmi <davschmi@student.ethz.ch>
2018-01-18 10:01:07 +0100

    refactored octree

--- include/octree/octree.hh
+++ include/octree/octree.hh
@@ -28,6 +28,7 @@
 namespace oct {
 
 // TODO(dave): write destructor
+// TODO(dave): use smart pointers
 
 class Octree {
  public:
@@ -38,25 +39,7 @@ class Octree {
     assert(leafsize > 0);
   }
 
-  void init() {
-    std::tuple<Eigen::Vector3d, precision_t> geometry_tuple(getgeometry());
-    _origin = std::get<0>(geometry_tuple);
-
-    const precision_t halfwidth(std::get<1>(geometry_tuple));
-
-    _root = new Octreenode(_origin, true, halfwidth, _treeinfo);
-
-    const unsigned int numpoints(_xpos.size());
-
-    // insert each point into tree
-    for (unsigned point_i = 0; point_i < numpoints; ++point_i) {
-      const precision_t x(_xpos(point_i));
-      const precision_t y(_ypos(point_i));
-      const precision_t z(_zpos(point_i));
-
-      _root->addpoint(point_i, x, y, z);
-    }
-  }
+  void init();
 
   const Octreenode* getroot() { return _root; }
 
@@ -65,22 +48,7 @@ class Octree {
   const array_t& _zpos;
 
  private:
-  std::tuple<Eigen::Vector3d, precision_t> getgeometry() {
-    const precision_t xorigin((_extent.x.second + _extent.x.first) / 2.0);
-    const precision_t yorigin((_extent.y.second + _extent.y.first) / 2.0);
-    const precision_t zorigin((_extent.z.second + _extent.z.first) / 2.0);
-
-    const precision_t xwidth((_extent.x.second - _extent.x.first));
-    const precision_t ywidth((_extent.y.second - _extent.y.first));
-    const precision_t zwidth((_extent.z.second - _extent.z.first));
-
-    const precision_t width(std::max(std::max(xwidth, ywidth), zwidth));
-
-    Eigen::Vector3d midpoint(xorigin, yorigin, zorigin);
-    auto geometry_tuple = std::make_tuple(midpoint, width * .5);
-
-    return geometry_tuple;
-  }
+  std::tuple<Eigen::Vector3d, precision_t> getgeometry();
 
   Eigen::Vector3d _origin;
   const Extent _extent;
